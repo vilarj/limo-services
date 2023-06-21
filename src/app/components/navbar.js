56b70e2824a93de43e77df255ab9c385948476cd
vilarj <lamaravillasan@outlook.com>
2023-06-21 12:49:56 -0400

    Adding routing to the navbar

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,3 +1,22 @@
+import Link from "next/link";
+
 export default function Navbar() {
-  return <div> navbar</div>;
+  return (
+    <nav>
+      <div>
+        <Link href="/">SOMOS</Link>
+      </div>
+      <ul>
+        <li>
+          <Link href="../about/">About</Link>
+        </li>
+        <li>
+          <Link href="../services/">Services</Link>
+        </li>
+        <li>
+          <Link href="../contact/">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
